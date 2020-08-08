728015197995a969f847a78fb0aec308fba672fc
nawomisawa <nawo2828@gmail.com>
2020-08-08 19:30:38 +0900

    [add]product-details-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'items#index'
-  resources :items, only: [:show]
+  resources :items, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
