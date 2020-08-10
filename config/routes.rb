ee2d8205030c4a33bc3b1c3ead400d3bede9424c
garlic-lindaman <mr.garlicboy@gmail.com>
2020-08-10 11:13:43 +0900

    【update】routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: [:index, :show]
+  resources :items, only: [:index, :show, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :purchases, only: :new
-  resources :items, only: [:index, :new]
 end
