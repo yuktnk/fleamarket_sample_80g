0a1e4a23aa3ef816f78062e8dd5d610198e2d4a7
yuktnk <yuki0wl@gmail.com>
2020-08-12 01:36:17 +0900

    [update]Test sign-in and sign-up with RSpec

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、全角カタカナのみで入力して下さい"}
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、全角カタカナのみで入力して下さい"}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX, message: "＠とドメインを含む必要があります"}
+  validates :nickname, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 7 }
+  validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "全角のみで入力して下さい"}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "全角のみで入力して下さい"}
   validates :family_name_furigana, presence: true, format: {with: /\A[ぁ-んァ-ヶー－]+\z/, message: "全角ひらがな、全角カタカナのみで入力して下さい"}
   validates :first_name_furigana, presence: true, format: {with: /\A[ぁ-んァ-ヶー－]+\z/, message: "全角ひらがな、全角カタカナのみで入力して下さい"}
   validates :birth_day, presence: true
---
+++ spec/factories/users.rb
@@ -0,0 +1,23 @@
+FactoryBot.define do
+  factory :user do
+    nickname                         {"ふりまちゃん"}
+    email                            {"kkk@gmail.com"}
+    password                         {"00000000"}
+    password_confirmation            {"00000000"}
+    family_name                      {"田中"}
+    first_name                       {"裕輝"}
+    family_name_furigana             {"たなか"}
+    first_name_furigana              {"ゆうき"}
+    birth_day                        {"1900-05-31"}
+    family_name_to_deliver           {"田中"}
+    first_name_to_deliver            {"裕輝"}
+    family_name_to_deliver_furigana  {"たなか"}
+    first_name_to_deliver_furigana   {"ゆうき"}
+    postal_code                      {"0002222"}
+    prefecture                       {"埼玉県"}
+    municipalities                   {"戸田市"}
+    address                          {"上戸田うん丁目"}
+    building                         {"マンション田中101"}
+    phone_number                     {"09012341234"}
+  end
+end
