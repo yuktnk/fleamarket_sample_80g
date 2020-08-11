cda545cdb106e3d45b19778e0ca42cc06b9f7c46
yuktnk <yuki0wl@gmail.com>
2020-08-11 11:00:17 +0900

    [update]To be able to SignUp

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+  
   def index
   end
 
@@ -7,4 +9,12 @@ class ItemsController < ApplicationController
   
   def new
   end
+
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
