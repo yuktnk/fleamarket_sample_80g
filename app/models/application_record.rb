a0d9501796269664d311c752b83a33ea37e845b6
yuktnk <yuki0wl@gmail.com>
2020-08-11 12:14:49 +0900

    Revert "[update]To be able to SignUp"
    This reverts commit cda545cdb106e3d45b19778e0ca42cc06b9f7c46.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,12 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  # before_action :authenticate_user!
-  # before_action :configure_permitted_parameters, if: :devise_controller?
-
-  # protected
-
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  # end
-
 end
