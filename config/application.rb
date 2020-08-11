2270b2d60699643e4f8100e7a32fb934047a8208
yuktnk <yuki0wl@gmail.com>
2020-08-11 18:17:38 +0900

    [update]Error handling(FlashMessage)

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module FleamarketSample80g
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.i18n.default_locale = :ja
     config.load_defaults 6.0
     config.generators do |g|
       g.stylesheets false
