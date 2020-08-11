89f805bb581961f787d240109c42892a52f04ed6
yuktnk <yuki0wl@gmail.com>
2020-08-11 12:24:28 +0900

    Revert "[update]To be able to SignUp"
    This reverts commit cda545cdb106e3d45b19778e0ca42cc06b9f7c46.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth, if: :production?
-
 
   private
 
@@ -11,12 +9,7 @@ class ApplicationController < ActionController::Base
       password == Rails.application.credentials[:basic_auth][:pass]
     end
   end
-  
   def production?
     Rails.env.production?
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_furigana, :first_name_furigana, :birth_day, :family_name_to_deliver, :first_name_to_deliver, :family_name_to_deliver_furigana, :first_name_to_deliver_furigana, :postal_code, :prefecture, :municipalities, :address, :building, :phone_number])
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,4 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
-  
   def index
   end
 
@@ -9,12 +7,4 @@ class ItemsController < ApplicationController
   
   def new
   end
-
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
-
 end
