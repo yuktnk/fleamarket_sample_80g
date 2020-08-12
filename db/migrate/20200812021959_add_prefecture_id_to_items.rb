5c26db763ad16afe4d11805b957aeadfc1c04a66
munetomo <fishmans0120@gmail.com>
2020-08-12 11:26:04 +0900

    Revert "[add]prefecture_id column to items table"
    This reverts commit c91021c35fafef799c1c9ab5d19c6fa2947d5a66.

--- db/migrate/20200812021959_add_prefecture_id_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddPrefectureIdToItems < ActiveRecord::Migration[6.0]
-  def change
-    add_column :items, :prefecture_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_021959) do
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "explanation", null: false
-    t.integer "price", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "prefecture_id"
-  end
+ActiveRecord::Schema.define(version: 2020_08_09_103838) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
