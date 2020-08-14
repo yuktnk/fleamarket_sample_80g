008845474cae7f4f5b3a37f464d2a4a3d1fc040c
yuktnk <yuki0wl@gmail.com>
2020-08-14 15:37:42 +0900

    [feature]Image posting function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,19 +2,36 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
   
   def index
+    @items = Item.includes(:item_images).order('created_at DESC')
   end
 
   def show
   end
   
   def new
+    @item = Item.new
+    @item.item_images.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :explanation, item_images_attributes: [:src])
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  has_many :item_images
+  accepts_nested_attributes_for :item_images, allow_destroy: true
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :item_condition
