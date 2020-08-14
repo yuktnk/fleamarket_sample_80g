5897b26d2831268e45cbd3ba7d0601b7b1aa27a4
garlic-lindaman <mr.garlicboy@gmail.com>
2020-08-14 12:08:14 +0900

    [add] define new method

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,4 +1,9 @@
 class CreditCardsController < ApplicationController
+
+  require 'payjp'
+
   def new
+    card = Card.where(user_id: current_user.id)
+    redirect_to action: "show" if card.exists?
   end
 end
