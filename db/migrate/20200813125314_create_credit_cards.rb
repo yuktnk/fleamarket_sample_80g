32170cfe1aeb4e6807ef194d61e92ca691bc525c
garlic-lindaman <mr.garlicboy@gmail.com>
2020-08-14 12:07:47 +0900

    [create] credit_cardsテーブルの作成

--- db/migrate/20200813125314_create_credit_cards.rb
+++ db/migrate/20200813125314_create_credit_cards.rb
@@ -1,9 +1,9 @@
 class CreateCreditCards < ActiveRecord::Migration[6.0]
   def change
     create_table :credit_cards do |t|
-      t.references :user_id, null: false, foreign_key: true
-      t.string :customer_id, null: false
       t.string :card_id, null: false
+      t.string :customer_id, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
