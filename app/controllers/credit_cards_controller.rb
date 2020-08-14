a77bc9bcda0bde94d3d459c7347ccd766a8833d9
garlic-lindaman <mr.garlicboy@gmail.com>
2020-08-14 14:20:36 +0900

    [add] define pay method

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -3,7 +3,26 @@ class CreditCardsController < ApplicationController
   require 'payjp'
 
   def new
-    card = Card.where(user_id: current_user.id)
-    redirect_to action: "show" if card.exists?
+    creditcard = Creditcard.where(user_id: current_user.id)
+    redirect_to action: "show" if creditcard.exists?
   end
-end
+
+  def pay
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    if params['payjp-token'].blank?
+      redirect_to action: "new"
+    else
+      customer = Payjp::Customer.create(
+        email: current_user.email,
+        card: params['payjp-token'],
+        metadata: {user_id: current_user.id}
+      )
+      @creditcard = Creditcard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @creditcard.save
+        redirect_to action: "show"
+      else
+        redirect_to action: "pay"
+      end
+    end
+  end
+
