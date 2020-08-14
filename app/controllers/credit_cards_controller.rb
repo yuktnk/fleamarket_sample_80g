b6e83a99a73c72eeb534a39284f271aa38a282ed
garlic-lindaman <mr.garlicboy@gmail.com>
2020-08-14 21:52:19 +0900

    Update credit_cards_controller.rb

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -3,50 +3,50 @@ class CreditCardsController < ApplicationController
   require 'payjp'
 
   def new
-    creditcard = Creditcard.where(user_id: current_user.id)
-    redirect_to action: "show" if creditcard.exists?
+    # creditcard = Creditcard.where(user_id: current_user.id)
+    # redirect_to action: "show" if creditcard.exists?
   end
 
   def pay
-    Payjp.api_key = ["PAYJP_PRIVATE_KEY"]
-    if params['payjp-token'].blank?
-      redirect_to action: "new"
-    else
-      customer = Payjp::Customer.create(
-        email: current_user.email,
-        card: params['payjp-token'],
-        metadata: {user_id: current_user.id}
-      )
-      @creditcard = Creditcard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      if @creditcard.save
-        redirect_to action: "show"
-      else
-        redirect_to action: "pay"
-      end
-    end
+    # Payjp.api_key = ["PAYJP_PRIVATE_KEY"]
+    # if params['payjp-token'].blank?
+    #   redirect_to action: "new"
+    # else
+    #   customer = Payjp::Customer.create(
+    #     email: current_user.email,
+    #     card: params['payjp-token'],
+    #     metadata: {user_id: current_user.id}
+    #   )
+    #   @creditcard = Creditcard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+    #   if @creditcard.save
+    #     redirect_to action: "show"
+    #   else
+    #     redirect_to action: "pay"
+    #   end
+    # end
   end
 
   def delete
-    creditcard = Creditcard.where(user_id: current_user.id).first
-    if creditcard.blank?
-    else
-      Payjp.api_key = ['PAYJP_PRIVATE_KEY']
-      customer = Payjp::Customer.retrieve(creditcard.customer_id)
-      customer.delete
-      creditcard.delete
-    end
-      redirect_to action: "new"
+    # creditcard = Creditcard.where(user_id: current_user.id).first
+    # if creditcard.blank?
+    # else
+    #   Payjp.api_key = ['PAYJP_PRIVATE_KEY']
+    #   customer = Payjp::Customer.retrieve(creditcard.customer_id)
+    #   customer.delete
+    #   creditcard.delete
+    # end
+    #   redirect_to action: "new"
   end
 
   def show
-    creditcard = Creditcard.where(user_id: cuurent_user.id).first
-    if creditcard.blank?
-      redirect_to action: "new"
-    else
-      Payjp.api_key = ["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.creditcard.retrieve(creditcard.card_id)
-    end
-  end
+  #   creditcard = Creditcard.where(user_id: cuurent_user.id).first
+  #   if creditcard.blank?
+  #     redirect_to action: "new"
+  #   else
+  #     Payjp.api_key = ["PAYJP_PRIVATE_KEY"]
+  #     customer = Payjp::Customer.retrieve(card.customer_id)
+  #     @default_card_information = customer.creditcard.retrieve(creditcard.card_id)
+  #   end
+  # end
 end
 
