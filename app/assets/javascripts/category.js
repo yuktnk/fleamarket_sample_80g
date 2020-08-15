a33466df21ea62c1d5d671521b3f13fc3ab96bbc
munetomo <fishmans0120@gmail.com>
2020-08-15 18:04:13 +0900

    [add]category function to items/new.html.haml
    商品データ送信確認済み。
    孫カテゴリによってサイズ表示のありなしに対応

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,7 +1,8 @@
 $(function() {
   // カテゴリーセレクトボックスのオプションを作成
   function appendOption(category) {
-    let html = `<option value="${category.category}" data-category="${category.id}">${category.category}</option>`;
+    let html = `<option value="${category.id}">${category.category}</option>`;
+    // let html = `<option value="${category.category}" data-category="${category.id}">${category.category}</option>`;
     return html;
   }
   // 子カテゴリーの表示作成
@@ -9,8 +10,8 @@ $(function() {
     let childSelectHtml = '';
     childSelectHtml = `<div class='selectWrap__added' id='children_wrapper'>
                         <div class='selectWrapBox'>
-                          <select class="select" id="child_category" name="category_id">
-                            <option value="---" data-category="---">---</option>
+                          <select id="child_category" name="item[category_id]" class="select">
+                            <option value="">---</option>
                             ${insertHTML}
                           </select>
                         </div>
@@ -22,8 +23,8 @@ $(function() {
     let grandchildSelectHtml = '';
     grandchildSelectHtml = `<div class='selectWrap__added' id= 'grandchildren_wrapper'>
                               <div class='selectWrapBox'>
-                                <select class="select" id="grandchild_category" name="category_id">
-                                  <option value="---" data-category="---">---</option>
+                                <select id="grandchild_category" name="item[category_id]" class="select">
+                                  <option value="">---</option>
                                   ${insertHTML}
                                 </select>
                               </div>
@@ -31,20 +32,24 @@ $(function() {
     $('.Main__center__container__categoryWrapper').append(grandchildSelectHtml);
   }
   // 親カテゴリー選択後のイベント
-  $('#parent_category').on('change', function() {
-    let parentCategory = document.getElementById('parent_category').value; //選択された親カテゴリーの名前を取得
-    if (parentCategory != "選択してください"){ //親カテゴリーが初期値でないことを確認
+  $('#item_category_id').on('change', function() {
+  // $('#parent_category').on('change', function() {
+    let parentId = document.getElementById('item_category_id').value; //選択された親カテゴリーの名前を取得
+    // let parentCategory = document.getElementById('parent_category').value; //選択された親カテゴリーの名前を取得
+    if (parentId != ""){ //親カテゴリーが初期値でないことを確認
+    // if (parentCategory != "選択してください"){ //親カテゴリーが初期値でないことを確認
       $.ajax({
-        url: 'get_category_children',
+        url: '/items/get_category_children/',
+        // url: 'get_category_children',
         type: 'GET',
-        data: { parent_category: parentCategory },
+        data: { parent_id: parentId },
         dataType: 'json'
       })
       .done(function(children) {
         $('#children_wrapper').remove(); //親が変更された時、子以下を削除する
         $('#grandchildren_wrapper').remove();
         $('#size_wrapper').remove();
-        $('#brand_wrapper').remove();
+        // $('#brand_wrapper').remove();
         let insertHTML = '';
         children.forEach(function(child) {
           insertHTML += appendOption(child);
@@ -58,31 +63,34 @@ $(function() {
       $('#children_wrapper').remove(); //親カテゴリーが初期値になった時、子以下を削除する
       $('#grandchildren_wrapper').remove();
       $('#size_wrapper').remove();
-      $('#brand_wrapper').remove();
+      // $('#brand_wrapper').remove();
     }
   });
   // 子カテゴリー選択後のイベント
   $('.Main__center__container__categoryWrapper').on('change', '#child_category', function() {
-    let childId = $('#child_category option:selected').data('category'); //選択された子カテゴリーのidを取得する
-    if (childId != "選択してください"){ //子カテゴリーが初期値でないことを確認
+    let childId = document.getElementById('child_category').value; //選択された子カテゴリーのidを取得する
+    // let childId = $('#child_category option:selected').data('category'); //選択された子カテゴリーのidを取得する
+    if (childId != "" && childId != 46 && childId != 74 && childId != 134 && childId != 142 && childId != 147 && childId != 150 && childId != 158){ //子カテゴリーが初期値でないことを確認
+    // if (childId != "選択してください"){ //子カテゴリーが初期値でないことを確認
       $.ajax({
-        url: 'get_category_grandchildren',
+        url: '/items/get_category_grandchildren/',
+        // url: 'get_category_grandchildren',
         type: 'GET',
         data: { child_id: childId },
         dataType: 'json'
       })
       .done(function(grandchildren) {
-        console.log(grandchildren)
-        if (grandchildren.length != 0) {
+        // console.log(grandchildren)
+        // if (grandchildren.length != 0) {
           $('#grandchildren_wrapper').remove(); //子が変更された時、孫以下を削除する
           $('#size_wrapper').remove();
-          $('#brand_wrapper').remove();
+          // $('#brand_wrapper').remove();
           let insertHTML = '';
           grandchildren.forEach(function(grandchild) {
             insertHTML += appendOption(grandchild);
           });
           appendGrandchildrenBox(insertHTML);
-        }
+        // }
       })
       .fail(function() {
         alert('カテゴリー取得に失敗しました');
@@ -90,12 +98,13 @@ $(function() {
     }else{
       $('#grandchildren_wrapper').remove(); //子カテゴリーが初期値になった時、孫以下を削除する
       $('#size_wrapper').remove();
-      $('#brand_wrapper').remove();
+      // $('#brand_wrapper').remove();
     }
   });
   // サイズセレクトボックスのオプションを作成
   function appendSizeOption(size) {
-    let html = `<option value="${size.size}">${size.size}</option>`;
+    let html = `<option value="${size.id}">${size.size}</option>`;
+    // let html = `<option value="${size.size}">${size.size}</option>`;
     return html;
   }
   // サイズ・ブランド入力欄の表示作成
@@ -106,8 +115,8 @@ $(function() {
                         <span class='Main__center__container__required'>必須</span>
                         <div class='selectWrap__added--size'>
                           <div class='selectWrapBox'>
-                            <select class="select" id="size" name="size_id>
-                              <option value="---">---</option>
+                            <select id="size" name="item[size_id]" class="select">
+                              <option value="">---</option>
                               ${insertHTML}
                             <select>
                           </div>
@@ -117,10 +126,12 @@ $(function() {
   }
   // 孫カテゴリ選択後のイベント
   $('.Main__center__container__categoryWrapper').on('change', '#grandchild_category', function() {
-    let grandchildId = $('#grandchild_category option:selected').data('category'); //選択された孫カテゴリのidを取得
+    let grandchildId = document.getElementById('grandchild_category').value; //選択された孫カテゴリのidを取得
+    // let grandchildId = $('#grandchild_category option:selected').data('category'); //選択された孫カテゴリのidを取得
     if (grandchildId != "---"){ //孫カテゴリーが初期値でないことを確認
       $.ajax({
-        url: 'get_size',
+        url: '/items/get_size',
+        // url: 'get_size',
         type: 'GET',
         data: { grandchild_id: grandchildId },
         dataType: 'json'
@@ -128,7 +139,7 @@ $(function() {
       .done(function(sizes) {
         console.log(sizes)
         $('#size_wrapper').remove(); //孫が変更された時、サイズ欄以下を削除する
-        $('#brand_wrapper').remove();
+        // $('#brand_wrapper').remove();
         if (sizes.length != 0) {
           let insertHTML = '';
           sizes.forEach(function(size) {
@@ -142,7 +153,7 @@ $(function() {
       })
     }else{
       $('#size_wrapper').remove(); //孫カテゴリが初期値になった時、サイズ欄以下を削除する
-      $('#brand_wrapper').remove();
+      // $('#brand_wrapper').remove();
     }
   });
 });
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,24 +6,30 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
+    @category_id = @item.category_id
+    @category_parent = Category.find(@category_id).parent.parent
+    @category_child = Category.find(@category_id).parent
+    @category_grandchild = Category.find(@category_id)
   end
   
   def new
     @item = Item.new
     @item.item_images.new
+    @category_parent_array = Category.where(ancestry: nil)
     # セレクトボックスの初期値設定
-    @category_parent_array = ["選択してください"]
-    # データベースから親カテゴリのみ抽出して配列にする
-    Category.where(ancestry: nil).each do |parent|
-      @category_parent_array << parent.category
-    end
+    # @category_parent_array = ["選択してください"]
+    # # データベースから親カテゴリのみ抽出して配列にする
+    # Category.where(ancestry: nil).each do |parent|
+    #   @category_parent_array << parent.category
+    # end
   end
 
   # 以下全て、formatはjsonのみ
   # 親カテゴリが選択された後に動くアクション
   def get_category_children
     # 選択された親カテゴリに紐づく子カテゴリの配列を取得する
-    @category_children = Category.find_by(category: "#{params[:parent_category]}", ancestry: nil).children
+    @category_children = Category.find("#{params[:parent_id]}").children
   end
 
   # 子カテゴリが選択された後に動くアクション
@@ -34,13 +40,13 @@ class ItemsController < ApplicationController
 
   # 孫カテゴリが選択された後に動くアクション（サイズ）
   def get_size
-    selected_grandchild = Category.find("#{params[:grandchild_id]}") #孫カテゴリを取得
-    if related_size_parent = selected_grandchild.sizes[0] #孫カテゴリの親と紐づくサイズ（親）があれば取得
-      @sizes = related_size_parent.children #紐付いたサイズ（親）の子の配列を取得
+    selected_grandchild = Category.find("#{params[:grandchild_id]}") 
+    if related_size_parent = selected_grandchild.sizes[0] 
+      @sizes = related_size_parent.children
     else
-      selected_child = Category.find("#{params[:grandchild_id]}").parent #孫カテゴリの親を取得
-      if related_size_parent = selected_child.sizes[0] #孫カテゴリの親と紐づくサイズ（親）があれば取得
-        @sizes = related_size_parent.children #紐付いたサイズ（親）の子の配列を取得
+      selected_child = Category.find("#{params[:grandchild_id]}").parent
+      if related_size_parent = selected_child.sizes[0]
+        @sizes = related_size_parent.children
       end
     end
   end
@@ -63,7 +69,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :price, :explanation, item_images_attributes: [:src])
+    params.require(:item).permit(:name, :price, :explanation, :category_id, :size_id, item_images_attributes: [:src])
   end
 
 end
