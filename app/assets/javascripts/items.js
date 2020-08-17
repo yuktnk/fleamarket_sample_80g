ba76871c78f5e61a9a3e673991c2e2e4c5391ca4
yuktnk <yuki0wl@gmail.com>
2020-08-17 13:39:40 +0900

    [update]Multiple image posting function
    画像投稿欄にて画像を複数枚選択できる機能の搭載

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,68 +1,68 @@
-$(document).on('turbolinks:load', ()=> {
-  // 画像用のinputを生成する関数
-  const buildFileField = (num)=> {
-    const html = `<div data-index="${num}" class="js-file_group">
-                      <input class="js-file" type="file"
-                      name="item[item_images_attributes][${num}][src]"
-                      id="item_item_images_attributes_${num}_src">
-                  </div>`;
-    return html;
-  }
-  // プレビュー用のimgタグを生成する関数
-  const buildImg = (index, url)=> {
-    const html = `<div class="previewField">
-                    <img data-index="${index}" src="${url}" width="120px" height="120px">
-                      <div data-index="${index}" class="js-file_group">
-                      <div class="js-remove">削除</div>
-                    </div>
-                  </div>`;
-    return html;
-  }
+// $(document).on('turbolinks:load', ()=> {
+//   // 画像用のinputを生成する関数
+//   const buildFileField = (num)=> {
+//     const html = `<div data-index="${num}" class="js-file_group">
+//                       <input class="js-file" type="file"
+//                       name="item[item_images_attributes][${num}][src]"
+//                       id="item_item_images_attributes_${num}_src">
+//                   </div>`;
+//     return html;
+//   }
+//   // プレビュー用のimgタグを生成する関数
+//   const buildImg = (index, url)=> {
+//     const html = `<div class="previewField">
+//                     <img data-index="${index}" src="${url}" width="120px" height="120px">
+//                       <div data-index="${index}" class="js-file_group">
+//                       <div class="js-remove">削除</div>
+//                     </div>
+//                   </div>`;
+//     return html;
+//   }
 
-  // file_fieldのnameに動的なindexをつける為の配列
-  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-  // 既に使われているindexを除外
-  lastIndex = $('.js-file_group:last').data('index');
-  fileIndex.splice(0, lastIndex);
+//   // file_fieldのnameに動的なindexをつける為の配列
+//   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+//   // 既に使われているindexを除外
+//   lastIndex = $('.js-file_group:last').data('index');
+//   fileIndex.splice(0, lastIndex);
 
-  $('.hidden-destroy').hide();
-  $('.js-file').hide();
+//   $('.hidden-destroy').hide();
+//   $('.js-file').hide();
 
-  $('#image-box').on('change', '.js-file', function(e) {
-    const targetIndex = $(this).parent().data('index');
-    // ファイルのブラウザ上でのURLを取得する
-    const file = e.target.files[0];
-    const blobUrl = window.URL.createObjectURL(file);
+//   $('#image-box').on('change', '.js-file', function(e) {
+//     const targetIndex = $(this).parent().data('index');
+//     // ファイルのブラウザ上でのURLを取得する
+//     const file = e.target.files[0];
+//     const blobUrl = window.URL.createObjectURL(file);
 
-    // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
-    if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
-      img.setAttribute('src', blobUrl);
-    } else {  // 新規画像追加の処理
-      $('#previews').append(buildImg(targetIndex, blobUrl));
-      // fileIndexの先頭の数字を使ってinputを作る
-      $('#image-box').append(buildFileField(fileIndex[0]));
-      fileIndex.shift();
-      $(this).parent().hide();
-      // 末尾の数に1足した数を追加する
-      fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
-    }
-    // 画像が10枚になったら画像入力欄を隠す
-    // if (fileIndex[9] >= 20) {
-    //   $('.js-file').hide();
-    // }
-  });
+//     // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
+//     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
+//       img.setAttribute('src', blobUrl);
+//     } else {  // 新規画像追加の処理
+//       $('#image-box').append(buildImg(targetIndex, blobUrl));
+//       // fileIndexの先頭の数字を使ってinputを作る
+//       $('#image-box').append(buildFileField(fileIndex[0]));
+//       fileIndex.shift();
+//       $(this).parent().hide();
+//       // 末尾の数に1足した数を追加する
+//       fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+//     }
+//     // 画像が10枚になったら画像入力欄を隠す
+//     // if (fileIndex[9] >= 20) {
+//     //   $('.js-file').hide();
+//     // }
+//   });
 
-  $('#image-box').on('click', '.js-remove', function() {
-    const targetIndex = $(this).parent().data('index');
-    // 該当indexを振られているチェックボックスを取得する
-    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
-    // もしチェックボックスが存在すればチェックを入れる
-    if (hiddenCheck) hiddenCheck.prop('checked', true);
+//   $('#image-box').on('click', '.js-remove', function() {
+//     const targetIndex = $(this).parent().data('index');
+//     // 該当indexを振られているチェックボックスを取得する
+//     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+//     // もしチェックボックスが存在すればチェックを入れる
+//     if (hiddenCheck) hiddenCheck.prop('checked', true);
 
-    $(this).parent().remove();
-    $(`img[data-index="${targetIndex}"]`).remove();
+//     $(this).parent().remove();
+//     $(`img[data-index="${targetIndex}"]`).remove();
 
-    // 画像入力欄が0個にならないようにしておく
-    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
-  });
-});
+//     // 画像入力欄が0個にならないようにしておく
+//     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+//   });
+// });
