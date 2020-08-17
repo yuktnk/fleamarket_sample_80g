1e93201dd1f3a9682310db74e556bd24abdf1f3d
garlic-lindaman <mr.garlicboy@gmail.com>
2020-08-17 19:16:31 +0900

    [update] route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
   root 'items#index'
   get 'users/logout_path', to: 'users#logout'
   resources :users, only: [:show]
-  resources :credit_cards, only: :new
   resources :items, only: [:index, :show, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :purchases, only: :new
+  resources :credit_cards, only: [:new,:show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 end
