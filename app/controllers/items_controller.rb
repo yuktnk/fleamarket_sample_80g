82c63b37d214bacf02aecf18854248e636c76fab
munetomo <fishmans0120@gmail.com>
2020-08-18 15:31:15 +0900

    [update]items/index.html.haml
    新規投稿商品が新しい投稿から5件分表示されるようにしました。
    画像はmasterに商品出品がマージされてからやります。
    ピックアップブランドにレディースを持って来る予定です。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,12 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
   
   def index
+    @items = Item.limit(5).order('created_at DESC')
+    # 画像は田中さんがマージしてから
+    # @ladies_items = Item.where(category: 2).includes(:images).order("created_at DESC").limit(5)
+    # ピックアップカテゴリー用
   end
+  
 
   def show
   end
