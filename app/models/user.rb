9895488f8b0310fe53a81b3453d39e284995f524
nawomisawa <68097314+nawomisawa@users.noreply.github.com>
2020-08-18 11:44:21 +0900

    Revert "Revert "[WIP]商品出品機能(分担：三澤)""

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,6 @@ class User < ApplicationRecord
   validates :municipalities, presence: true
   validates :address, presence: true
 
-  # extend ActiveHash::Associations::ActiveRecordExtensions
-  # belongs_to_active_hash :prefecture
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 end
