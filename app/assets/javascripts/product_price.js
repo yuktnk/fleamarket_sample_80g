505cf999cf0544461fef9be0bea39a329c3166d0
nawomisawa <nawo2828@gmail.com>
2020-08-19 13:02:00 +0900

    [update]js
    explanetion_textcnt.jsとproduct_price.jsの向上化しようとしたが、解決できず、ほぼ元の形のままです。

--- app/assets/javascripts/product_price.js
+++ app/assets/javascripts/product_price.js
@@ -1,14 +1,35 @@
-$( document ).on( 'change', '#product_price', function(){
-  $("#product_price").keyup(function(){
-    var inputPrice = Number($(this).val());
-    if(inputPrice >= 300 && inputPrice < 10000000){
-      var fee = Math.floor(inputPrice * 0.1);
-      var benefit = inputPrice - fee;
-      $('#price_fee').text("¥" + fee.toLocaleString()),
-      $('#price_benefit').text("¥" + benefit.toLocaleString())
-    }else{
-      $('#price_fee').html("ー");
-      $('#price_benefit').html("ー");
-    }
+
+$(function(){    
+  $('#product_price').on('input', function(){
+    $(this).keyup(function(){
+      var inputPrice = Number($(this).val());
+      if(inputPrice >= 300 && inputPrice < 10000000){
+        var fee = Math.floor(inputPrice * 0.1);
+        var benefit = inputPrice - fee;
+        $('#price_fee').text("¥" + fee.toLocaleString()),
+        $('#price_benefit').text("¥" + benefit.toLocaleString())
+      }else{
+        $('#price_fee').html("ー");
+        $('#price_benefit').html("ー");
+      }
+    });
   });
 });
+
+
+
+// 参考元コード
+// $( document ).on( 'input', '#product_price', function(){
+//   $("#product_price").keyup(function(){
+//     var inputPrice = Number($(this).val());
+//     if(inputPrice >= 300 && inputPrice < 10000000){
+//       var fee = Math.floor(inputPrice * 0.1);
+//       var benefit = inputPrice - fee;
+//       $('#price_fee').text("¥" + fee.toLocaleString()),
+//       $('#price_benefit').text("¥" + benefit.toLocaleString())
+//     }else{
+//       $('#price_fee').html("ー");
+//       $('#price_benefit').html("ー");
+//     }
+//   });
+// });
