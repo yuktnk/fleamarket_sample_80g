58d43674364d2c1200324cb92602fce48cc244ef
garlic-lindaman <mr.garlicboy@gmail.com>
2020-08-19 14:45:35 +0900

    [update]payjp.js
    コメント編集のみ

--- app/assets/ javascripts/payjp.js
+++ app/assets/ javascripts/payjp.js
@@ -4,10 +4,11 @@ document.addEventListener(
     if (document.getElementById("token_submit") != null) {
       //token_submitというidがnullの場合、下記コードを実行しない
       Payjp.setPublicKey("pk_test_0cab5a53778ebb4cb97de57f"); //公開鍵
-      let btn = document.getElementById("token_submit"); //IDがtoken_submitの場合に取得されます
+      let btn = document.getElementById("token_submit"); //IDがtoken_submitの場合に取得される
       btn.addEventListener("click", (e) => {
-        //ボタンが押されたときに作動します
-        e.preventDefault(); //ボタンを一旦無効化します
+        //ボタンが押されたときに作動する
+        e.preventDefault(); //ボタンを一旦無効化しとる
+        // カード情報の作成
         let card = {
           number: document.getElementById("card_number").value,
           cvc: document.getElementById("cvc").value,
@@ -16,13 +17,13 @@ document.addEventListener(
         }; //入力されたデータを取得します。
         Payjp.createToken(card, (status, response) => {
           if (status === 200) {
-            //成功した場合
+            //成功した場合、200になるらしい
             $("#card_number").removeAttr("name");
             $("#cvc").removeAttr("name");
             $("#exp_month").removeAttr("name");
             $("#exp_year").removeAttr("name"); //データを自サーバにpostしないように削除
             $("#card_token").append(
-              $('<input type="hidden" name="payjp-token">').val(response.id)
+              $('<input type= "hidden" name= "payjp-token">').val(response.id)
             ); //取得したトークンを送信できる状態にします
             document.inputForm.submit();
             alert("登録が完了しました"); //確認用
