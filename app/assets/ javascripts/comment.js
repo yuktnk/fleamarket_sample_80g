118221c002ea9a9c1e0f559394c181e8763135ab
yuktnk <yuki0wl@gmail.com>
2020-08-19 22:23:27 +0900

    [create]comment.js

---
+++ app/assets/ javascripts/comment.js
@@ -0,0 +1,25 @@
+$(function(){
+  $('#new_comment').on('submit', function(e){
+    e.preventDefault();
+    console.log(this);
+    let formData = new FormData(this);
+    let url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.comments').append(html);
+      $('.textbox').val('');
+      $('.form__submit').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+})
