b1591dc716c5cd2de2dc4d976b621c18025a3868
nawomisawa <nawo2828@gmail.com>
2020-08-20 14:40:25 +0900

    [add]test_image.jpg
    テスト用の画像をpublic/imagesに配置した

---
+++ spec/models/item_image_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Item_image do
+  describe '#create' do
+
+    # 商品の画像が必須
+    it "is invalid without a item_image" do
+      item_image = build(:item_image, src: "")
+      item_image.valid?
+      expect(item_image.errors[:src]).to include("を入力してください")
+    end
+
+  end
+end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,75 @@
 require 'rails_helper'
 describe Item do
+
+  describe '#create' do
+
+    # # 商品の画像が必須
+    # it "is invalid without a item_images" do
+    #   item = build(:item, item_images: "")
+    #   item.valid?
+    #   expect(item.errors[:item_images]).to include("を入力してください")
+    # end
+
+    # 商品の名前が必須
+    it "is invalid without a name" do
+      item = build(:item, name: "")
+      item.valid?
+      expect(item.errors[:neme]).to include("を入力してください")
+    end
+
+    # 商品の説明が必須
+    it "is invalid without a explanation" do
+      item = build(:item, explanation: "")
+      item.valid?
+      expect(item.errors[:explanation]).to include("を入力してください")
+    end
+
+    # 商品のカテゴリが必須
+    it "is invalid without a category_id" do
+      item = build(:item, category_id: "")
+      item.valid?
+      expect(item.errors[:category_id]).to include("を入力してください")
+    end
+
+    # 商品のブランドが任意
+    
+    # 商品の状態が必須
+    it "is invalid without a item_condition_id" do
+      item = build(:item, item_condition_id: "")
+      item.valid?
+      expect(item.errors[:item_condition_id]).to include("を入力してください")
+    end
+
+    # 商品の配送料が必須
+    it "is invalid without a delivery_fee_id" do
+      item = build(:item, delivery_fee_id: "")
+      item.valid?
+      expect(item.errors[:delivery_fee_id]).to include("を入力してください")
+    end
+
+    # 商品の配送先が必須
+    it "is invalid without a prefecture_id" do
+      item = build(:item, prefecture_id: "")
+      item.valid?
+      expect(item.errors[:prefecture_id]).to include("を入力してください")
+    end
+
+    # 商品の発送日数が必須
+    it "is invalid without a preparation_day_id" do
+      item = build(:item, preparation_day_id: "")
+      item.valid?
+      expect(item.errors[:preparation_day_id]).to include("を入力してください")
+    end
+
+    # 商品の価格が必須
+    it "is invalid without a price" do
+      item = build(:item, price: "")
+      item.valid?
+      expect(item.errors[:price]).to include("を入力してください")
+    end
+
+  end
+
   describe '#search' do
     it "keyがない場合、全ての商品レコードは表示できる" do
       item = create(:item)
@@ -27,4 +97,6 @@ describe Item do
       expect(described_class.search(key).length).to eq 0
     end
   end
+
+
 end
