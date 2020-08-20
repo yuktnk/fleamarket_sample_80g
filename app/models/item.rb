6dea7fddae05e0259ab8dd144a6a6fc62a0f2b12
yuktnk <yuki0wl@gmail.com>
2020-08-20 17:16:34 +0900

    [update]Refactoring

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   has_many :comments
-  has_many :item_images
+  has_many :item_images, dependent: :destroy
   belongs_to :category
   accepts_nested_attributes_for :item_images, allow_destroy: true
   
