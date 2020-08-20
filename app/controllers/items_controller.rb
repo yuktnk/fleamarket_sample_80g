94ccc83e079f4dbbca384df9e593acd5630070b6
yuktnk <yuki0wl@gmail.com>
2020-08-20 18:16:59 +0900

    [update]items_controller(before_action)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show, :search]
   before_action :category_parent_array, only: [:new, :create, :edit, :update]
+  before_action :set_item, only[:show, :destroy]
   
   def index
     @items = Item.includes(:item_images).limit(3).order('created_at DESC')
@@ -50,7 +51,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
     @comment = Comment.new
     @comments = @item.comments.includes(:user)
     @category_grandchild = @item.category
@@ -63,9 +63,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    if item.destroy
+    @item.destroy
+    if @item.destroy
       redirect_to root_path
       flash[:notice] = "商品を削除しました"
     else
@@ -90,4 +89,9 @@ class ItemsController < ApplicationController
   def category_parent_array
     @category_parent_array = Category.where(ancestry: nil)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
