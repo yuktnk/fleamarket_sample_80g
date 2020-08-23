82493fef5eb802d0ebcf58be077cc9f3469265d7
nawomisawa <nawo2828@gmail.com>
2020-08-23 22:41:08 +0900

    [update]models/user.rb
    class User < ActiveRecord::Baseとこれに付随するendを消去

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  class User < ActiveRecord::Base
-    has_many :bought_items, class_name: 'Item', foreign_key: 'buyer_id'
-    has_many :sold_items, class_name: 'Item', foreign_key: 'seller_id'
-  end
+  has_many :bought_items, class_name: 'Item', foreign_key: 'buyer_id'
+  has_many :sold_items, class_name: 'Item', foreign_key: 'seller_id'
   
 
   has_many :comments
