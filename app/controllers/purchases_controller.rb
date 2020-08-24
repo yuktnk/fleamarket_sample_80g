a52740f864798ebd1a412ec6695a8447782b5ad4
garlic-lindaman <mr.garlicboy@gmail.com>
2020-08-24 16:26:13 +0900

    [update]credit_cards_controller.rb
    鍵の呼び出しをcredentialへ対応するように変更

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -13,7 +13,7 @@ class PurchasesController < ApplicationController
       #登録された情報がない場合にカード登録画面に移動する
       redirect_to controller: "credit_cards", action: "new"
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = Rails.application.credentials.pay_jp[:PAYJP_PRIVATE_KEY]
       #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(@credit_card.customer_id)
       #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
@@ -24,7 +24,7 @@ class PurchasesController < ApplicationController
   def pay
 
     @item = Item.find(params[:price])
-    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    Payjp.api_key = Rails.application.credentials.pay_jp[:PAYJP_PRIVATE_KEY]
     Payjp::Charge.create(
     amount: @item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
     customer: @credit_card.customer_id, #顧客ID
