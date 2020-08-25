1dd1141563d834618df0db0773c598985740531d
garlic-lindaman <mr.garlicboy@gmail.com>
2020-08-25 18:54:21 +0900

    [updete]items_controller.rb
    before_action: set_itemに
    :done追加
    
    また、doneメソッド内の記述を削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
 
   before_action :move_to_index, except: [:index, :show, :search]
   before_action :category_parent_array, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:show, :destroy, :purchase]
+  before_action :set_item, only: [:show, :destroy, :purchase, :done]
   
   def index
     @items = Item.includes(:item_images).limit(3).order('created_at DESC')
@@ -46,7 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def done
-    @item = Item.find(params[:id])
   end
 
   
